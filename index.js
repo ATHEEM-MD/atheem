eaf439d4b301ae80a916702f4081e4429280d32f
ATHEEM-MD <hathumani430@gmail.com>
2025-09-10 10:46:54 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "name": "pairing-code-generator",
+  "version": "1.0.0",
+  "description": "WhatsApp Pairing Code Generator using Baileys",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "type": "module",
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.6.0",
+    "@hapi/boom": "^10.0.0",
+    "pino": "^8.0.0",
+    "qrcode-terminal": "^0.12.0"
+  }
+}
